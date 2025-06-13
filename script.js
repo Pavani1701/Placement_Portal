e1a303583c99ed04b33311163313bab93d29ae56
Pavani Madhyannapu <110667498+Pavani1701@users.noreply.github.com>
2025-06-13 18:14:21 -0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('applicationForm').addEventListener('submit', function (e) {
-  e.preventDefault();
-  document.getElementById('successMessage').style.display = 'block';
-  this.reset();
-});
